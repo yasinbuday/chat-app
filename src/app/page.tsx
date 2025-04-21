251c0a7720cfc770b789dad8dc17baabdacb57f6
yasinbuday <yasin2003kerem@gmail.com>
2025-04-21 18:48:37 +0300

    add auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 
 
 export default async function Home() {
-
-  await db.set('hello', 'hello')
-  return <div className="text-red-500">Hello World</div>;
+  return <Button>Hello</Button>;
 }
