cd429e1980d1fbe0d48906612c61cf4f8522875f
yasinbuday <yasin2003kerem@gmail.com>
2025-05-21 20:41:51 +0300

    fixing errors

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { signIn } from "next-auth/react";
 import { FC, useState } from "react";
 import {toast} from "react-hot-toast"
