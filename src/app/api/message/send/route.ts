177adc07351a2ce1292d66ddb8ddf678f556d992
yasinbuday <yasin2003kerem@gmail.com>
2025-05-21 22:45:18 +0300

    initial commit

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -47,13 +47,13 @@ export async function POST(req: Request) {
 
     const message = ({} = messageValidator.parse(messageData));
 
-    pusherServer.trigger(
+    await pusherServer.trigger(
       toPusherKey(`chat:${chatId}`),
       "incoming-message",
       message
     );
 
-    pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), "new_message", {
+     await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), "new_message", {
       ...message,
       senderImg: sender.image,
       senderName: sender.name,
